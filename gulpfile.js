9b80dadbe7c77aad50fb7b09a79e41e2dbcd81af
rusakovsb <rusakov@inbox.ru>
2020-03-24 15:19:36 +0300

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+
+const cssFiles = [
+    './src/css/main.css',
+    './src/css/grid.css',
+    './src/css/views.css',
+    './src/css/colors.css',
+    './src/css/responsive.css'
+]
+
+function styles() {
+    return gulp.src(cssFiles)
+    .pipe(autoprefixer({
+        browsers: ['last 10 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('./css'))
+}
+
+function watch() {
+    gulp.watch('./src/css/**/*.css', styles);
+}
+
+gulp.task('styles', styles);
+gulp.task('watch', watch);
+gulp.task('dev', gulp.series('styles', 'watch'));
