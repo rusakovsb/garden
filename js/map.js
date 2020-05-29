69b4080fc724bee5e1739df859498c95516c48e1
rusakovsb <rusakov@inbox.ru>
2020-05-29 12:59:37 +0300

    updated logo, map, contact classes and styles

--- js/map.js
+++ js/map.js
@@ -2,157 +2,157 @@
 	 
     $(".map-point__number").hover(
         function () {
-            $(this).next(".map-point__title").toggleClass("map-point__title--hidden map-point__title--visible");
+            $(this).next(".map-point__title").toggleClass("map-point__title_hidden map-point__title_visible");
         }
     );	
 	
     $("#legend-item-1").hover(
         function () {
-            $("#map-point-1 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-1 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 	
     $("#legend-item-2").hover(
         function () {
-            $("#map-point-2 .map-point__number, #map-point-2_2 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-2 .map-point__number, #map-point-2_2 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 	
     $("#legend-item-3").hover(
         function () {
-            $("#map-point-3 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-3 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 	
     $("#legend-item-4").hover(
         function () {
-            $("#map-point-4 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-4 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 	
     $("#legend-item-5").hover(
         function () {
-            $("#map-point-5 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-5 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 	
     $("#legend-item-6").hover(
         function () {
-            $("#map-point-6 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-6 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 		
     $("#legend-item-7").hover(
         function () {
-            $("#map-point-7 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-7 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 		
     $("#legend-item-8").hover(
         function () {
-            $("#map-point-8 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-8 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 			
     $("#legend-item-9").hover(
         function () {
-            $("#map-point-9 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-9 .map-point__number").toggleClass("map-point__number_active");
        }
     );	
 			
     $("#legend-item-10").hover(
         function () {
-            $("#map-point-10 .map-point__number, #map-point-10_2 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-10 .map-point__number, #map-point-10_2 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 			
     $("#legend-item-11").hover(
         function () {
-            $("#map-point-11 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-11 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 			
     $("#legend-item-12").hover(
         function () {
-            $("#map-point-12 .map-point__number, #map-point-12_2 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-12 .map-point__number, #map-point-12_2 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 			
     $("#legend-item-13").hover(
         function () {
-            $("#map-point-13 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-13 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 			
     $("#legend-item-14").hover(
         function () {
-            $("#map-point-14 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-14 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 				
     $("#legend-item-15").hover(
         function () {
-            $("#map-point-15 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-15 .map-point__number").toggleClass("map-point__number_active");
         }
     );
 				
     $("#legend-item-16").hover(
         function () {
-            $("#map-point-16 .map-point__number, #map-point-16_2 .map-point__number, #map-point-16_3 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-16 .map-point__number, #map-point-16_2 .map-point__number, #map-point-16_3 .map-point__number").toggleClass("map-point__number_active");
         }
     );		
 				
     $("#legend-item-17").hover(
         function () {
-            $("#map-point-17 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-17 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 	
     $("#legend-item-18").hover(
         function () {
-            $("#map-point-18 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-18 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 	
     $("#legend-item-19").hover(
         function () {
-            $("#map-point-19 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-19 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 	
     $("#legend-item-20").hover(
         function () {
-            $("#map-point-20 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-20 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 	
     $("#legend-item-21").hover(
         function () {
-            $("#map-point-21 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-21 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 	
     $("#legend-item-22").hover(
         function () {
-            $("#map-point-22 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-22 .map-point__number").toggleClass("map-point__number_active");
        }
       );
 	
     $("#legend-item-23").hover(
         function () {
-            $("#map-point-23 .map-point__number").toggleClass("map-point__number--active");	  
+            $("#map-point-23 .map-point__number").toggleClass("map-point__number_active");	  
         }
     );	
 	
     $("#legend-item-24").hover(
         function () {
-            $("#map-point-24 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-24 .map-point__number").toggleClass("map-point__number_active");
         }
     );
 	
     $("#legend-item-25").hover(
         function () {
-            $("#map-point-25 .map-point__number").toggleClass("map-point__number--active");
+            $("#map-point-25 .map-point__number").toggleClass("map-point__number_active");
         }
     );	
 	
